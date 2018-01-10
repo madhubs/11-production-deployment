74aa777d21e88a28f86e8655eb73a3b8edb9140c
Madeline Stevens <stevensm1987@gmail.com>
2018-01-09 19:48:45 -0800

    pushing what I have for 11, haven't deployed yet, making branch for lab 12 page.js

---
+++ lab-maddy/client/scripts/models/book.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var app = app || {};
+var __API_URL__ = 'https://books301.herokuapp.com'; // TODO: Define the URL to your deployed API on Heroku
+
+(function(module) {
+  function errorCallback(err) {
+    console.error(err);
+    module.errorView.initErrorPage(err);
+  }
+
+  function Book(rawBookObj) {
+    Object.keys(rawBookObj).forEach(key => this[key] = rawBookObj[key]);
+  }
+
+  Book.prototype.toHtml = function() {
+    let template = Handlebars.compile($('#book-list-template').text());
+    return template(this);
+  }
+
+  Book.all = [];
+  Book.loadAll = rows => Book.all = rows.sort((a, b) => b.title - a.title).map(book => new Book(book));
+
+  Book.fetchAll = callback =>
+    $.get(`${__API_URL__}/api/v1/books`)
+      .then(Book.loadAll)
+      .then(callback)
+      .catch(errorCallback);
+
+  module.Book = Book;
+})(app)
---
+++ lab-maddy/server/server.js
@@ -0,0 +1,29 @@
+'use strict'
+
+// Application dependencies
+const express = require('express');
+const cors = require('cors');
+const pg = require('pg');
+
+// Application Setup
+const app = express();
+const PORT = process.env.PORT;
+const CLIENT_URL = process.env.CLIENT_URL;
+
+// Database Setup
+const client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
+client.on('error', err => console.error(err));
+
+// Application Middleware
+app.use(cors());
+
+// API Endpoints
+app.get('/api/v1/books', (req, res) => {
+  client.query(`SELECT book_id, title, author, image_url, isbn FROM books;`)
+    .then(results => res.send(results.rows))
+    .catch(console.error);
+});
+
+app.get('*', (req, res) => res.redirect(CLIENT_URL));
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
